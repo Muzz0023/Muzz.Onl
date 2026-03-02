ce294fff0677150e8164454eb2a5cac5de4c7be2
Muzz0023 <lauchy23@outlook.com>
2026-03-02 17:53:14 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: false,
-  },
+    rollupOptions: {
+      external: ['@revenuecat/purchases-capacitor']
+    }
+  }
 })
